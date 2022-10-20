4af8a7719e453ac9fbf0123bc870a26c63ef03a8
Caetano César <116229164+XxxCaetanoxxX@users.noreply.github.com>
2022-10-20 01:16:15 +0000

    usando formatação

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ nome=str(input("Qual é o seu nome? "))
 idade=int(input("Quantos anos você tem? "))
 print("Olá",nome,"sua idade é:",idade,"anos.")
 
+print("Minha idade é "+str(idade))
+print(f"Minha idade é {idade}\n")
+print("Minha idade é {}".format(idade))
+
+#Quando quiser usar mais de uma variável usando o .format: 
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
